0d14814e34f55baffffe0afdcd94a41d8ad3ada0
minha <miraculum95@gmail.com>
2022-09-29 21:13:34 +0900

    Feat: 게시글 태그 기능 추가

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,21 +1,79 @@
 from rest_framework import serializers
-from rest_framework.response import Response
 
-from posts.models import Post
+from posts.models import Post, Tag
+
 
 class PostListSerializer(serializers.ModelSerializer):
     like = serializers.IntegerField(default=0, read_only=True)
     view = serializers.IntegerField(default=0, read_only=True)
-    image = serializers.ImageField(use_url=True)
-    
+    tag = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Post
+        ordering = ['-id']
+        fields = '__all__'
+        
+class CreateSerializer(serializers.ModelSerializer):
+    '''
+    게시물 생성
+    NOTICE: tag 값이 None으로 나온다.
+    '''
+    like = serializers.IntegerField(default=0, read_only=True)
+    view = serializers.IntegerField(default=0, read_only=True)
+    tag = serializers.CharField()
+    image = serializers.ImageField(use_url=True, default=None)
+
     class Meta:
         model = Post
         fields = '__all__'
-
+   
+    # 게시물 작성
     def create(self, validated_data):
-        post = Post.objects.create(**validated_data)
+        if 'image' in validated_data.keys():
+            p1 = Post(
+                title = validated_data['title'],
+                content = validated_data['content'],
+                image = validated_data['image']
+            )
+        else:
+            p1 = Post(
+                title = validated_data['title'],
+                content = validated_data['content']
+            )
+        p1.save()
+        tag_data = validated_data['tag']
+        result = 'True'
+        # for key, value in tag.items():
+        for v in tag_data.split(','):
+            if v.startswith('#'):
+                for i in Tag.objects.all():
+                    if v == i.name:
+                        p1.tag.add(i)
+                        p1.save()
+                        result = 'False'
+                        break
+                if result =='False':
+                    result = 'True'
+                    continue
+                t1 = Tag(name=v)
+                t1.save()
+                p1.tag.add(t1)
+                p1.save()
+        return p1
 
-        return post
+        # t1 = Tag(validated_data['tag'])
+        # t1.save()
+        # p1 = Post(
+        #     title = validated_data['title'],
+        #     content = validated_data['content']
+        # )
+        # p1.save()
+        # p1.tag.add(t1)
+        # p1.save()
+        # return p1
+ 
+
+
 # class PostRetrieveSerializer(serializers.ModelSerializer):
 #     category = serializers.StringRelatedField()
 #     tags = serializers.StringRelatedField(many=True)
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from posts.views import PostViewSet
+from posts.views import PostViewSet, CreateViewSet
 
 urlpatterns = [
-    path('', PostViewSet.as_view(actions={'get': 'list', 'post': 'create'})),
-    path('<int:pk>/', PostViewSet.as_view(actions={'get': 'view','patch': 'partial_update', 'delete': 'destroy'})),
-]
+    path('', PostViewSet.as_view(actions={'get': 'list'})), # 메인페이지
+    path('board/', CreateViewSet.as_view()), # 게시글 작성 페이지
+    path('<int:pk>/', PostViewSet.as_view(actions={'get': 'view','patch': 'partial_update', 'delete': 'destroy'})), # 상세페이지
+] 
