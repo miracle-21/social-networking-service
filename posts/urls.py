7c5e0f8cad318ada73a009ec52b82f4b4a8c0840
minha <miraculum95@gmail.com>
2022-09-30 23:13:32 +0900

    Docs: README 작성

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from posts.views import *
 
 urlpatterns = [
     path('', PostViewSet.as_view(actions={'get': 'list'})), # 메인페이지
-    path('<int:pk>/', PostDetailListAPIView.as_view()),
+    path('<int:pk>/', PostDetailListAPIView.as_view()), # 상세페이지
     path('board/', CreateViewSet.as_view()), # 게시글 작성 페이지
     path('comment/', CommentCreateAPIView.as_view()), # 댓글 생성
     path('comment/<int:pk>/', CommentUpdateAPIView.as_view()), # 댓글 수정/삭제
